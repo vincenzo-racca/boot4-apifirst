4504b4f4a6f0844983fda4d9ee9f66187fd0d391
Vincenzo Racca <vincenzo.racca22@gmail.com>
2026-06-27 17:44:32 +0200

    edit README.md
    (cherry picked from commit 79c853a367b98fd102a0a1fbf14e011d49c697d1)

--- src/main/java/com/vincenzoracca/boot4/Boot4ApifirstApplication.java
+++ src/main/java/com/vincenzoracca/boot4/Boot4ApifirstApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot4Application {
+public class Boot4ApifirstApplication {
 
 	void main(String[] args) {
-		SpringApplication.run(Boot4Application.class, args);
+		SpringApplication.run(Boot4ApifirstApplication.class, args);
 	}
 
 }
